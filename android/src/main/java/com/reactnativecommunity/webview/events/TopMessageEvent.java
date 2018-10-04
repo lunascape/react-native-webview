65a2cb37f465f094e61f29f69491037e081d8a69
ankur-sardar <ankur.sardar18@gmail.com>
2018-10-04 15:31:14 +0900

    support captureScreen feature in android webview

--- android/src/main/java/com/reactnativecommunity/webview/events/TopMessageEvent.java
+++ android/src/main/java/com/reactnativecommunity/webview/events/TopMessageEvent.java
@@ -8,36 +8,51 @@ import com.facebook.react.uimanager.events.RCTEventEmitter;
 /**
  * Event emitted when there is an error in loading.
  */
-public class TopMessageEvent extends Event<TopMessageEvent> {
 
-  public static final String EVENT_NAME = "topMessage";
-  private WritableMap mData;
+public class TopMessageEvent {
+  public static final String CAPTURE_SCREEN_EVENT_NAME = "captureScreen";
+  public static final String ON_MESSAGE_EVENT_NAME = "topMessage";
 
-  public TopMessageEvent(int viewId, WritableMap data) {
-    super(viewId);
-    mData = data;
+  public static PBWebViewGenericEvent createMessageEvent(int viewId, WritableMap eventData) {
+    return new PBWebViewGenericEvent(viewId, ON_MESSAGE_EVENT_NAME, eventData);
+  }
+  public static PBWebViewGenericEvent createCaptureScreenEvent(int viewId, WritableMap eventData) {
+    return new PBWebViewGenericEvent(viewId, CAPTURE_SCREEN_EVENT_NAME, eventData);
   }
 
-  @Override
-  public String getEventName() {
-    return EVENT_NAME;
-  }
+  static class PBWebViewGenericEvent extends Event<PBWebViewGenericEvent> {
 
-  @Override
-  public boolean canCoalesce() {
-    return false;
-  }
+    public static final String EVENT_NAME = "topMessage";
+    private WritableMap mData;
+    public String eventName;
 
-  @Override
-  public short getCoalescingKey() {
-    // All events for a given view can be coalesced.
-    return 0;
-  }
+    public PBWebViewGenericEvent(int viewId, String eventName, WritableMap data) {
+      super(viewId);
+      mData = data;
+      this.eventName = eventName;
+    }
 
-  @Override
-  public void dispatch(RCTEventEmitter rctEventEmitter) {
-    // WritableMap data = Arguments.createMap();
-    // data.putString("data", mData);
-    rctEventEmitter.receiveEvent(getViewTag(), EVENT_NAME, mData);
+    @Override
+    public String getEventName() {
+      return this.eventName;
+    }
+
+    @Override
+    public boolean canCoalesce() {
+      return false;
+    }
+
+    @Override
+    public short getCoalescingKey() {
+      // All events for a given view can be coalesced.
+      return 0;
+    }
+
+    @Override
+    public void dispatch(RCTEventEmitter rctEventEmitter) {
+      // WritableMap data = Arguments.createMap();
+      // data.putString("data", mData);
+      rctEventEmitter.receiveEvent(getViewTag(), getEventName(), mData);
+    }
   }
 }
